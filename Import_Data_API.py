d8498b173d45ecf097f0b2d447cfeead52980883
Alexandre <alexandre.rau@students.fhnw.ch>
2019-11-28 11:02:50 +0100

    added correct file

--- Import_Data_API.py
+++ Import_Data_API.py
@@ -4,19 +4,18 @@ from influxdb import DataFrameClient, InfluxDBClient
 
 #add pandas version control 0.24
 
-from DB_Fill import DB_HOST, DB_PORT, DB_DBNAME
+from config import DB_HOST, DB_PORT, DB_DBNAME
 
-client = InfluxDBClient(host = DB_HOST, port = DB_PORT)
+client = DataFrameClient(host = DB_HOST, port = DB_PORT, database = DB_DBNAME)
 
-db_name = 'meteorology'
-protocol = 'line'
+#print("Created Mythenquai DataFrame")
+#df_1 = pd.read_csv('./data/messwerte_mythenquai_2019.csv')
+#df_1.info()
 
-print("Created Mythenquai DataFrame")
-df_1 = pd.read_csv('./data/messwerte_mythenquai_2019.csv')
-df_1.info()
-
-print("Created Tiefenbrunnen DataFrame")
-df_2 = pd.read_csv('./data/messwerte_tiefenbrunnen_2019.csv')
-df_2.info()
+#print("Created Tiefenbrunnen DataFrame")
+#df_2 = pd.read_csv('./data/messwerte_tiefenbrunnen_2019.csv')
+#df_2.info()
 
 client.get_list_database()
+
+client.switch_database(DB_DBNAME)
