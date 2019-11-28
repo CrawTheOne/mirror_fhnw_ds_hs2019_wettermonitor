3862d0bdd70a41f7ca379d86150af2d75c7844b5
Alexandre <alexandre.rau@students.fhnw.ch>
2019-11-28 11:18:13 +0100

    upgrading

--- Import_Data_API.py
+++ Import_Data_API.py
@@ -4,9 +4,9 @@ from influxdb import DataFrameClient, InfluxDBClient
 
 #add pandas version control 0.24
 
-from config import DB_HOST, DB_PORT, DB_DBNAME
+import config
 
-client = DataFrameClient(host = DB_HOST, port = DB_PORT, database = DB_DBNAME)
+client = DataFrameClient(host = config.DB_HOST, port = config.DB_PORT, database = config.DB_DBNAME)
 
 #print("Created Mythenquai DataFrame")
 #df_1 = pd.read_csv('./data/messwerte_mythenquai_2019.csv')
@@ -17,5 +17,9 @@ client = DataFrameClient(host = DB_HOST, port = DB_PORT, database = DB_DBNAME)
 #df_2.info()
 
 client.get_list_database()
+client.switch_database(config.DB_DBNAME)
 
-client.switch_database(DB_DBNAME)
+query = "SELECT COUNT(air_temperature) FROM \"{}\",\"{}\" tz('Europe/Zurich')".format(config.stations[0], config.stations[1])
+result = client.query(query)
+print(result[config.stations[0]])
+print(result[config.stations[1]])
