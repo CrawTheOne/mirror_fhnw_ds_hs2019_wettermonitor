1d7dab257b3c52c40996c0243f05ca9ae5827aff
Alexandre <alexandre.rau@students.fhnw.ch>
2019-11-29 15:00:54 +0100

    up to date stuff

--- Import_Data_API.py
+++ Import_Data_API.py
@@ -5,16 +5,13 @@ import pytz
 #add pandas version control 0.24
 
 import config
-<<<<<<< HEAD
 
 client = DataFrameClient(host = config.DB_HOST, port = config.DB_PORT, database = config.DB_DBNAME)
 
 start_time = '2019-10-01T00:00:00+00:00'
 end_time = '2019-11-28T00:00:00+00:00'
-=======
 
 client = DataFrameClient(host = config.DB_HOST, port = config.DB_PORT, database = config.DB_DBNAME)
->>>>>>> 3862d0bdd70a41f7ca379d86150af2d75c7844b5
 
 # NoSQL Query  (to be added: timezone adjusting)
 query = "SELECT * FROM \"{}\",\"{}\" WHERE time >= '{}' AND time <= '{}' "\
@@ -22,19 +19,18 @@ query = "SELECT * FROM \"{}\",\"{}\" WHERE time >= '{}' AND time <= '{}' "\
 
 df_air_temp = client.query(query)
 
-<<<<<<< HEAD
 # to create pandas df, use only one dicitonary part (mythenquai, tiefenbrunnen)
 mythenquai_nov_dez = pd.DataFrame(df_air_temp['mythenquai'])
 mythenquai_nov_dez.info()
 
 tiefenbrunnen_nov_dez = pd.DataFrame(df_air_temp['tiefenbrunnen'])
 tiefenbrunnen_nov_dez.info()
-=======
-client.get_list_database()
-client.switch_database(config.DB_DBNAME)
 
-query = "SELECT COUNT(air_temperature) FROM \"{}\",\"{}\" tz('Europe/Zurich')".format(config.stations[0], config.stations[1])
-result = client.query(query)
-print(result[config.stations[0]])
-print(result[config.stations[1]])
->>>>>>> 3862d0bdd70a41f7ca379d86150af2d75c7844b5
+
+
+#client.get_list_database()
+#client.switch_database(config.DB_DBNAME)
+#query = "SELECT COUNT(air_temperature) FROM \"{}\",\"{}\" tz('Europe/Zurich')".format(config.stations[0], config.stations[1])
+#result = client.query(query)
+#print(result[config.stations[0]])
+#print(result[config.stations[1]])
