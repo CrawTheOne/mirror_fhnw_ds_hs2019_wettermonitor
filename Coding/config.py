e992b32c5889f1d2ad90a9c0a43d0f4da62c37e1
Alexandre <alexandre.rau@students.fhnw.ch>
2019-12-06 13:06:39 +0100

    starting object oriented coding

--- Coding/config.py
+++ Coding/config.py
@@ -8,3 +8,11 @@ stations = ['mythenquai', 'tiefenbrunnen']
 print(DB_HOST +":"+str(DB_PORT))
 
 days_delta = 365
+
+class connection:
+
+    def __init__(self, name):
+        self.name = name
+
+    def change_name(self, new_name):
+        self.name = new_name
